757a3a920fd562f465bf41afca9048069d2ad077
kasya-pedestrain <lixinyuan951224@gmail.com>
2023-01-16 21:51:45 +0900

    start HelloWorld

---
+++ src/jp/feichaixiaobao/user/li/sample1_1.java
@@ -0,0 +1,9 @@
+package jp.feichaixiaobao.user.li;
+
+public class sample1_1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.print("hello world");
+        System.out.printf("hello world");
+    }
+}

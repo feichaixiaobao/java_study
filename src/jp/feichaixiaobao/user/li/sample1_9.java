01b53e6810d7e05c0530cef3e36b73ea1ee5ae43
废柴小宝 <122708531+feichaixiaobao@users.noreply.github.com>
2023-01-19 00:44:11 +0900

    Update sample1_9.java

--- src/jp/feichaixiaobao/user/li/sample1_9.java
+++ src/jp/feichaixiaobao/user/li/sample1_9.java
@@ -1,9 +1,11 @@
 package jp.feichaixiaobao.user.li;
 
+// こういうような課題ごとに、課題の内容をコメントしてくれればうれしい。 by.小宝
 //練習問題 1 - 9
 //int 型の変数 x に任意の数値を代入し、x を 2 倍、3 倍、4 倍した結果を表示するプログラムを作成しなさい。
 
 
+// 不要な改行を削除したほうがいい。 by.小宝
 public class sample1_9 {
     public static void main(String[] args) {
 
@@ -15,4 +17,5 @@ public class sample1_9 {
 
 
 
+// 不要な改行を削除したほうがいい。 by.小宝
 }

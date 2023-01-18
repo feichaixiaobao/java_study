1b8c493a865ff92347adc188af7719f8a6a687ff
1HAOHAOXUE <dajiashihaopengyou@gmail.com>
2023-01-18 09:49:31 +0900

    ganbadie first java

---
+++ src/jp/feichaixiaobao/user/ganbadie/HelloWorldhaha.java
@@ -0,0 +1,11 @@
+package jp.feichaixiaobao.user.ganbadie;
+
+import static java.lang.System.out;
+
+public class hello {
+    public static void main(String[] args) {
+        out.println("Hello World!");
+        out.print("Hello World!");
+        out.printf("Hello World!");
+    }
+}

afff8d4b78236753f72a0992448e333c91c0b0ad
废柴小宝 <122708531+feichaixiaobao@users.noreply.github.com>
2023-01-19 00:00:43 +0900

    Update sample1_11.java

--- src/jp/feichaixiaobao/user/hyz3125/sample1_11.java
+++ src/jp/feichaixiaobao/user/hyz3125/sample1_11.java
@@ -7,6 +7,9 @@ public class sample1_11 {
     //x を x より小さい任意の数値で割った商と余りを表示するプログラムを作成しなさい。
     int x = (int)(Math.random() * 10);
     int y = (int)(Math.random() * 10 - 1);
+    
+    // Math.random()で乱数を取得しているので、yはxより大きい場合がある。 by.小宝
+        
     System.out.println("x=" + x / y);
     System.out.println("x=" + x % y);
     }

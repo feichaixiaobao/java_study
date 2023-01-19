d116f6d837f3fec4133e849dbb097b2c28568170
dongjihui666 <dongjihui0320@gmail.com>
2023-01-19 22:31:18 +0900

    add xiaohui sample1_1

---
+++ src/jp/dongxiaohui/user/sample1_1.java
@@ -0,0 +1,11 @@
+package jp.dongxiaohui.user;
+
+/*
+“Hello World”と表示するプログラムを作成しなさい。
+ */
+public class sample1_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

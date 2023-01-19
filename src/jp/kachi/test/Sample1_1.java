f05d856ad9ceebf9a380a717d91f16e1eec15e0c
kakai2023 <koi20223@gmail.com>
2023-01-19 16:24:21 +0800

    delete test

--- src/jp/kachi/test/Sample1_1.java
+++
@@ -1,8 +0,0 @@
-package jp.kachi.test;
-
-public class Sample1_1 {
-	public static void main(String[] args) {
-		System.out.println("Hello Wolrd!!");
-	}
-
-}
--- src/jp/kachi/test/Sample1_5.java
+++
@@ -1,8 +0,0 @@
-package jp.kachi.test;
-
-public class Sample1_5 {
-	public static void main(String[] args) {
-		System.out.println(13 * 17);
-	}
-
-}

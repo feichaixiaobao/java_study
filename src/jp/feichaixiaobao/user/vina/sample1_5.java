16900c9d9d4c2adfa1d671a899f171e427ac4d4d
syoho <syoho.xiaofan@gmail.com>
2023-01-20 22:52:48 +0800

    練習問題 1 - 5

---
+++ src/jp/feichaixiaobao/user/vina/sample1_5.java
@@ -0,0 +1,14 @@
+
+/*
+練習問題 1 - 5
+数値 13 と 17 の積を表示するプログラムを作成しなさい。ただし、変数を使用しないこと。
+※System.out.println()の引数部分で演算を行う。
+*/
+
+package jp.feichaixiaobao.user.vina;
+
+public class sample1_5 {
+    public static void main(String[] args) {
+        System.out.println(13 * 17) ;
+    }
+}

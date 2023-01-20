f80a53b9630d9805ead52f9f006a7c690241f058
ethanlmx <ethanlmx@outlook.com>
2023-01-20 20:20:19 +0900

    課題1-10を提出

--- src/jp/feichaixiaobao/user/ethanlmx/sample1_10.java
+++ src/jp/feichaixiaobao/user/ethanlmx/sample1_10.java
@@ -3,6 +3,8 @@ package jp.feichaixiaobao.user.ethanlmx;
 public class sample1_10 {
 	public static void main(String[] args) {
 		int x = 11;
-		System.out.printf("x=%d", x);
+		System.out.println(x);
+		System.out.println(x * x);
+		System.out.println(x * x * x);
 	}
 }

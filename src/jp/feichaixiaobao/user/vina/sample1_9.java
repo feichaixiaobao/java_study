f5bd1adbb3bde82678e92bd47b888d23fa0dc6ef
vina <code.tokyo@qq.com>
2023-01-21 16:22:19 +0800

    練習問題 1 - 9

---
+++ src/jp/feichaixiaobao/user/vina/sample1_9.java
@@ -0,0 +1,16 @@
+
+/*
+練習問題 1 - 9
+int 型の変数 x に任意の数値を代入し、x を 2 倍、3 倍、4 倍した結果を表示するプログラムを作成しなさい。
+*/
+
+package jp.feichaixiaobao.user.vina;
+
+public class sample1_9 {
+    public static void main(String[] args) {
+        int x = 100 ;
+        System.out.println( x * 2 );
+        System.out.println( x * 3 );
+        System.out.println( x * 4 );
+    }
+}

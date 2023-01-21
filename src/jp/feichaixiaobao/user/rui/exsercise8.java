f0ba183d8c37da28945425a493ec8f8469a855cc
TZT <wandoumilk@gmail.com>
2023-01-22 01:13:11 +0900

    add rui sample8

---
+++ src/jp/feichaixiaobao/user/rui/exsercise8.java
@@ -0,0 +1,12 @@
+package jp.feichaixiaobao.user.rui;
+
+public class exsercise8{
+
+	public static void main(String[] args) {
+		int x = 19;
+		int y = 23;
+		int z = x * y;
+		System.out.println("z =" + z);
+	}
+
+}

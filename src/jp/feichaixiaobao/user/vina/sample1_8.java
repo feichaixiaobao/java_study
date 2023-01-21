7068786f312c6c51e23207c6724429575918f414
vina <code.tokyo@qq.com>
2023-01-21 11:04:13 +0800

    練習問題 1 - 8

---
+++ src/jp/feichaixiaobao/user/vina/sample1_8.java
@@ -0,0 +1,17 @@
+
+/*
+練習問題 1 - 8
+int 型の変数 x、y に数値 19、23 を代入し、その積を変数 z に代入して z の値を表示するプログラムを作成しなさい。
+*/
+
+package jp.feichaixiaobao.user.vina;
+
+public class sample1_8 {
+    public static void main(String[] args) {
+        int x,y,z ;
+        x = 19 ;
+        y = 23 ;
+        z = x * y ;
+        System.out.println("z=" + z);
+    }
+}

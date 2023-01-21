43cb7b143a9fecf65ec1dc7eab3cbd7ff811247d
TZT <wandoumilk@gmail.com>
2023-01-22 01:12:46 +0900

    add rui sample7

---
+++ src/jp/feichaixiaobao/user/rui/exsercise7.java
@@ -0,0 +1,15 @@
+package jp.feichaixiaobao.user.rui;
+
+public class exsercise7 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int z;
+		z = x;
+		x = y;
+		y = z;
+		System.out.println("x=" + x + ",y=" + y);
+	}
+
+}

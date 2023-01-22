bc0b9918ff623d71226e6ff59899e5957677a439
vina <code.tokyo@qq.com>
2023-01-22 16:57:16 +0800

    練習問題 1 - 11

---
+++ src/jp/feichaixiaobao/user/vina/sample1_11.java
@@ -0,0 +1,14 @@
+
+/*
+練習問題 1 - 11
+int 型の変数 x に任意の数値を代入し、x を x より小さい任意の数値で割った商と余りを表示するプログラムを作成しなさい。
+*/
+
+package jp.feichaixiaobao.user.vina;
+
+public class sample1_11 {
+    public static void main(String[] args) {
+        int x = 100 ;
+        System.out.println( "商:" + ( x / 15) + ";余り:" + ( x % 15));
+    }
+}

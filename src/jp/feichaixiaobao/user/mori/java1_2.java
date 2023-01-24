da11273c1ab1fb3c79c49c97994ca3ac1279c4c2
MoriYang <123165112+MoriYang@users.noreply.github.com>
2023-01-24 17:30:52 +0900

    Add java1_2 sample

---
+++ src/jp/feichaixiaobao/user/mori/java1_2.java
@@ -0,0 +1,16 @@
+package jp.feichaixiaobao.user.mori;
+
+public class java1_2 {
+	public static void main(String[] args) {
+		System.out.println("----menthod 1----");
+		int x = 11;
+		
+			System.out.printf("x=%d",x);
+		System.out.println(" ");	
+		System.out.println("----menthod 2----");
+		int a = 11;
+			System.out.print("x=");
+			System.out.print(a);
+	}
+
+}

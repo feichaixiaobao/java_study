31c20aad6aabb4ec6eecb9c913646081be438287
vina <code.tokyo@qq.com>
2023-01-24 15:12:24 +0800

    練習問題 2 - 3

---
+++ src/jp/feichaixiaobao/user/vina/sample2_3.java
@@ -0,0 +1,22 @@
+
+/*
+練習問題 2 - 3
+int 型の変数 x に数値を入力し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
+*/
+
+package jp.feichaixiaobao.user.vina;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sample2_3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferInt = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(bufferInt.readLine());
+        System.out.println( x );
+        System.out.println( x * x );
+        System.out.println( x * x * x);
+    }
+}
+

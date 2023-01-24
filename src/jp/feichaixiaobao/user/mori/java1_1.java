1c622e85d101b7c02ea6d9f9b2b54abb93af6f50
MoriYang <123165112+MoriYang@users.noreply.github.com>
2023-01-24 16:48:48 +0900

    Add java1_1 sample

---
+++ src/jp/feichaixiaobao/user/mori/java1_1.java
@@ -0,0 +1,15 @@
+package jp.feichaixiaobao.user.mori;
+
+public class java1_1 {
+	
+		public static void main(String[] args) {
+				System.out.println("Hello World!");
+			String hw="hello world!!";
+				System.out.printf(hw);
+				System.out.println();
+				System.out.print("Hello");
+				System.out.print(" ");
+				System.out.print("World!!!");
+	}
+
+}

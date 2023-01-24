321c318394c1b0351a69d59f6be0dae85f798b77
Ykkjava <670983085@qq.com>
2023-01-24 09:43:19 +0900

    Add Sample1_1.java

---
+++ src/jp/feichaixiaobao/user/yojava/Sample1_1.java
@@ -0,0 +1,15 @@
+package jp.feichaixiaobao.user.yojava;
+
+
+/*
+ * 練習問題 1 - 1
+“Hello World”と表示するプログラムを作成しなさい。
+※System.out.println()、System.out.print()、System.out.printf()などを使用する。
+ */
+public class Sample1_1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world!!!");
+	}
+
+}

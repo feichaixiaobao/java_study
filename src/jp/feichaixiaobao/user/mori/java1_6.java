94cb6afb5f5c4338acae4e99eb431d57f3f0954e
MoriYang <123165112+MoriYang@users.noreply.github.com>
2023-01-25 23:05:03 +0900

    Add java1_6 sample

---
+++ src/jp/feichaixiaobao/user/mori/java1_6.java
@@ -0,0 +1,14 @@
+package jp.feichaixiaobao.user.mori;
+
+public class java1_6 {
+
+	public static void main(String[] args) {
+		int x = 7;
+		System.out.println("x =" +" "+x * 3);
+		
+		System.out.println("x =" +  x * 0.5);
+		
+
+	}
+
+}

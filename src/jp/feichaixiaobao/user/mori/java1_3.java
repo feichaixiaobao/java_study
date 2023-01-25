2b7d9f2724e4ba4daff0ae02e9a7ba48f5849914
MoriYang <123165112+MoriYang@users.noreply.github.com>
2023-01-25 22:53:39 +0900

    Add java1_3 sample

---
+++ src/jp/feichaixiaobao/user/mori/java1_3.java
@@ -0,0 +1,20 @@
+package jp.feichaixiaobao.user.mori;
+
+public class java1_3 {
+
+	public static void main(String[] args) {
+		System.out.println("----method 1----");
+		int x = 13;
+		int y = 14;
+		System.out.print("x=");
+		System.out.print(x);
+		System.out.print(",");
+		System.out.print("y=");
+		System.out.println(y);
+		
+		System.out.println("----method 12----");
+		System.out.printf("x=%d,y=%d",x,y);
+
+	}
+
+}

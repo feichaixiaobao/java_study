3e0c98b9721bb491776f1137454e1a4ac41d6b81
MoriYang <123165112+MoriYang@users.noreply.github.com>
2023-01-26 19:21:37 +0900

    Add java1_7 sample

---
+++ src/jp/feichaixiaobao/user/mori/java1_7.java
@@ -0,0 +1,23 @@
+package jp.feichaixiaobao.user.mori;
+
+public class java1_7 {
+	public static class sample1_7 {
+		/*
+		練習問題 1 - 7
+		int 型の変数 x、y に任意の数値を代入し、
+		x の値を y に、y の値を x に入れ替えて x と y の値を表示するプログラムを作成しなさい。
+		※xとy以外に、もうひとつ変数が必要となる。
+		*/
+		
+		public static void main(String[] args) {
+	        int x = 100 ;
+	        int y = 200 ;
+	        int z;
+	        z = x ;
+	        x = y ;
+	        y = z ;
+	        System.out.println("x=" + x + ",y=" + y);
+	    }
+	}
+
+}

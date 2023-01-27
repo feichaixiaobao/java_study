a883ff975b019557502fe9ef534ef555ebd0a575
MoriYang <123165112+MoriYang@users.noreply.github.com>
2023-01-27 13:51:52 +0900

    add java1_11 sample

---
+++ src/jp/feichaixiaobao/user/mori/java1_11.java
@@ -0,0 +1,15 @@
+package jp.feichaixiaobao.user.mori;
+
+public class java1_11 {
+
+	public static void main(String[] args) {
+		/*
+		 * int 型の変数 x に任意の数値を代入し、
+		 * x を x より小さい任意の数値で割った商と余りを表示するプログラムを作成しなさい。
+		 */
+		int x = 7;
+		System.out.println("X / 3 =" + " " +x/3 + "......" + x%3 );
+
+	}
+
+}

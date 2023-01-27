895861571c2601da0937f1fb22e0856afcb41667
MoriYang <123165112+MoriYang@users.noreply.github.com>
2023-01-27 13:51:28 +0900

    add java1_10 sample

---
+++ src/jp/feichaixiaobao/user/mori/java1_10.java
@@ -0,0 +1,17 @@
+package jp.feichaixiaobao.user.mori;
+
+public class java1_10 {
+
+	public static void main(String[] args) {
+		/*
+		 * int 型の変数 x に任意の数値を代入し、
+		 * x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
+		 */
+		int x = 2;
+		System.out.println("x*1 =" + " " +  x * 1);
+		System.out.println("x*2 =" + " " +  x * 2);
+		System.out.println("x*3 =" + " " +  x * 3);
+
+	}
+
+}

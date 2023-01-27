821dc13b5ed9dd2c773b6842d8aee356254f842e
MoriYang <123165112+MoriYang@users.noreply.github.com>
2023-01-27 13:52:07 +0900

    add java1_12 sample

---
+++ src/jp/feichaixiaobao/user/mori/java1_12.java
@@ -0,0 +1,24 @@
+package jp.feichaixiaobao.user.mori;
+
+public class java1_12 {
+
+	public static void main(String[] args) {
+		/*
+		 * int 型の変数 x に任意の数値を代入し、
+		 * インクリメント演算子、デクリメント演算子を適用して結果を表示することにより
+		 * 演算子の効果を確認するプログラムを作成しなさい。
+		 */
+		int num = 5;
+		num++;
+
+		System.out.println(num);  // 6 が出力される
+		
+		num--;
+
+		System.out.println(num);  // 5 が出力される
+
+		
+
+	}
+
+}

0c0d45cc048c1bd24c977911a0fd3bf2cc929247
Yuwen <u06m3jp600@gmail.com>
2023-01-29 20:36:39 +0900

    sample 2-1 から 2-6

---
+++ src/jp/feichaixiaobao/user/uuwen/sample2_6.java
@@ -0,0 +1,19 @@
+package jp.feichaixiaobao.user.uuwen;
+import java.util.Scanner;
+
+/*
+練習問題 2 - 6
+年齢を訊ね、生まれてから現在までの、おおよその日数を表示するプログラムを作成しなさい。
+※日数の計算は閏年等を考慮せず、年齢×365とする。
+ */
+public class sample2_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("年齢を入力:");
+        int age = scanner.nextInt();
+
+
+        System.out.println("生まれてから現在までの日数 = " + (age*365) + "日");
+    }
+}

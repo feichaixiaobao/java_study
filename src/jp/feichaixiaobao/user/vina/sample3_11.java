ee99cc351d40111a34c076cf6ecf33d0bcec4e64
vina <code.tokyo@qq.com>
2023-02-05 21:47:39 +0800

    練習問題 3 - 11 - 修正

--- src/jp/feichaixiaobao/user/vina/sample3_11.java
+++ src/jp/feichaixiaobao/user/vina/sample3_11.java
@@ -32,35 +32,48 @@ public class sample3_11 {
             case 1:
                 System.out.println("元日" );
                 System.out.println("成人の日");
+                break;
             case 2:
                 System.out.println("建国記念の日");
+                break;
             case 3:
                 System.out.println("春分の日");
+                break;
             case 4:
                 System.out.println("昭和の日");
+                break;
             case 5:
                 System.out.println("憲法記念日");
                 System.out.println("みどりの日");
                 System.out.println("こどもの日");
+                break;
             case 6:
                 System.out.print("祝日がない");
+                break;
             case 7:
                 System.out.println("海の日");
+                break;
             case 8:
                 System.out.print("祝日がない");
+                break;
             case 9:
                 System.out.println("敬老の日");
                 System.out.println("秋分の日");
+                break;
             case 10:
                 System.out.println("体育の日");
+                break;
             case 11:
                 System.out.println("文化の日" );
                 System.out.println("勤労感謝の日");
+                break;
             case 12:
                 System.out.println("天皇誕生日");
+                break;
         }
     }
 }
+
 
 
 

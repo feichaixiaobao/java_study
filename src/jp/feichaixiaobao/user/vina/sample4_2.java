28ef29f3f1da04e3f20393f5bb23e962f2a93e03
vina <code.tokyo@qq.com>
2023-02-07 11:48:59 +0800

    練習問題 4 - 2

---
+++ src/jp/feichaixiaobao/user/vina/sample4_2.java
@@ -0,0 +1,19 @@
+
+/*
+練習問題 4 - 2
+九九、三の段（ 3 ～ 27 の 3 の倍数）を表示するプログラムを作成しなさい。
+*/
+
+package jp.feichaixiaobao.user.vina;
+
+public class sample4_2 {
+    public static void main(String[] args) {
+        for ( int i = 1 ; i <= 9 ; i++){
+            System.out.println( 3 * i );
+        }
+    }
+}
+
+
+
+

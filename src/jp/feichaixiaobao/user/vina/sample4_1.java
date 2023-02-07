615443e0a1e59a98738181b15abe14529ef9889a
vina <code.tokyo@qq.com>
2023-02-07 11:45:18 +0800

    練習問題 4 - 1

---
+++ src/jp/feichaixiaobao/user/vina/sample4_1.java
@@ -0,0 +1,19 @@
+
+/*
+練習問題 4 - 1
+“SPAM”という単語を 10 回表示するプログラムを作成しなさい。
+*/
+
+package jp.feichaixiaobao.user.vina;
+
+public class sample4_1 {
+    public static void main(String[] args) {
+        for ( int i = 0 ; i < 10 ; i ++) {
+            System.out.println( "SPAM" );
+        }
+    }
+}
+
+
+
+

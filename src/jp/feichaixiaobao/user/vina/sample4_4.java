eb541a4fe4683bd22f55346812f1ee68f284e05b
vina <code.tokyo@qq.com>
2023-02-10 22:26:05 +0800

    練習問題 4 - 4

---
+++ src/jp/feichaixiaobao/user/vina/sample4_4.java
@@ -0,0 +1,23 @@
+
+/*
+練習問題 4 - 4
+7 の階乗を計算し、表示するプログラムを作成しなさい。
+*/
+
+package jp.feichaixiaobao.user.vina;
+
+public class sample4_4 {
+    public static void main(String[] args) {
+        int x = 1;
+        for ( int i = 1 ; i <= 7 ; i++ ){
+            x = x * i;
+        }
+        System.out.println(x);
+    }
+}
+
+
+
+
+
+

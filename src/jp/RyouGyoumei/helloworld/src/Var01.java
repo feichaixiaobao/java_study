6d6cd28403635e3ba478b491a9808b3798a96716
ryougyoumei <ryougyoumei@gmail.com>
2023-02-12 18:14:04 +0900

    Third times submission

---
+++ src/jp/RyouGyoumei/helloworld/src/Var01.java
@@ -0,0 +1,16 @@
+public class Var01 {
+    public static void main(String[] args) {
+        
+        int age = 30;
+        double score = 88.9;
+        char gender = '男';
+        String name = "Tom";
+
+        System.out.println("人的信息如下：");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(score);
+        System.out.println(gender);
+
+    }
+}

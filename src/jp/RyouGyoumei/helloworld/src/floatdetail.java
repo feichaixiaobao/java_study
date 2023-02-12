521a708b9dfc7f6f42e9bd76a2f38e40f7fc2460
ryougyoumei <ryougyoumei@gmail.com>
2023-02-12 23:07:40 +0900

    floatdetail practice

--- src/jp/RyouGyoumei/helloworld/src/floatdetail.java
+++ src/jp/RyouGyoumei/helloworld/src/floatdetail.java
@@ -1,8 +1,21 @@
 public class floatdetail {
 
     public static void main(String[] args) {
-        float num1 = 1.1f;
-        System.out.println(num1);
+        double num11 = 2.7;
+        double num12 = 2.7;
+        System.out.println(num11);
+        System.out.println(num12);
+
+        if ( num11 == num12) {
+            System.out.println("num11 == num12 相等");
+        }
+
+        if (Math.abs(num11 - num12) < 0.000001 ) {
+            System.out.println("差值非常小，到我的规定精度，认为相等");
+            
+        }
+
+        System.out.println(Math.abs(num11 - num12));
     }
     
 }

673377bdc27039bdba1d2c7b3cc4195eae517788
ryougyoumei <ryougyoumei@gmail.com>
2023-02-12 22:36:30 +0900

    TEST

--- src/jp/RyouGyoumei/helloworld/src/Vardetail.java
+++ src/jp/RyouGyoumei/helloworld/src/Vardetail.java
@@ -4,6 +4,6 @@ public class Vardetail {
         System.out.println("100" + 98);
         System.out.println(100 + 3 + "hello");
         System.out.println("hello" + 100 + 3);     
-        
+        System.out.println("hello" + 1055 + 3);    
     }
 }
---
+++ src/jp/RyouGyoumei/helloworld/src/floatdetail.java
@@ -0,0 +1,9 @@
+public class floatdetail {
+
+    public static void main(String[] args) {
+        float num1 = 1.1f;
+        System.out.println(num1);
+    }
+    
+}
+

902c5060f5a478d98f86343c685c7d1a573af8f7
ryougyoumei <ryougyoumei@gmail.com>
2023-02-13 00:11:09 +0900

    P52

---
+++ src/jp/RyouGyoumei/helloworld/src/AutoConvert.java
@@ -0,0 +1,8 @@
+public class AutoConvert {
+    public static void main(String[] args) {
+        int num = 'a';
+        double d1 = 80;
+        System.out.println(num);
+        System.out.println(d1);
+    }
+}

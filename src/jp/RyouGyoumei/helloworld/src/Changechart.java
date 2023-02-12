310790182142b7bf74bbb3371985b82c14cf14ad
ryou <ryougyoumei@gmail.com>
2023-02-12 09:59:40 +0900

    First submission

---
+++ src/jp/RyouGyoumei/helloworld/src/Changechart.java
@@ -0,0 +1,10 @@
+public class Changechart {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Beijing\tHongkong\tShenzhen");
+        System.out.println("Beijing\nHongkong\nShenzhen");
+        System.out.println("Beijing\\Hongkong\\Shenzhen");
+        System.out.println("\"Beijing\\Hongkong\\Shenzhen\"");
+        System.out.println("Beijing\rHongkong\rShenzhen");
+        System.out.println("title\tauthor\tprice\tsales\nHarry\tTom\t100¥\t200");
+    }
+}
---
+++ src/jp/RyouGyoumei/helloworld/src/Helloworld.java
@@ -0,0 +1,6 @@
+public class Helloworld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, ryou!");
+    }
+}
+

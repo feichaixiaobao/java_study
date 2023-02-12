3848becf733d16eaa36f5b1b1c56e27cc9b4bd9e
ryougyoumei <ryougyoumei@gmail.com>
2023-02-12 22:35:03 +0900

    update

---
+++ src/jp/RyouGyoumei/helloworld/src/Vardetail.java
@@ -0,0 +1,9 @@
+public class Vardetail {
+    public static void main(String[] args) {
+        System.out.println(100 + 98);
+        System.out.println("100" + 98);
+        System.out.println(100 + 3 + "hello");
+        System.out.println("hello" + 100 + 3);     
+        
+    }
+}

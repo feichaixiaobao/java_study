94422e1f75503e942cdd7fedec4774bfa5084110
ryougyoumei <ryougyoumei@gmail.com>
2023-02-14 09:14:09 +0900

    next

--- src/jp/RyouGyoumei/helloworld/src/AutoConvert.java
+++ src/jp/RyouGyoumei/helloworld/src/AutoConvert.java
@@ -4,5 +4,14 @@ public class AutoConvert {
         double d1 = 80;
         System.out.println(num);
         System.out.println(d1);
+
+        int n1 = 10;
+        float d2 = n1 + 1.1F;
+        byte b1 = 10;
+
+        byte b2 = 1;
+        byte b3 = 2;
+        short s1 = 1;
+        int s2 = b2 + s1;
     }
 }

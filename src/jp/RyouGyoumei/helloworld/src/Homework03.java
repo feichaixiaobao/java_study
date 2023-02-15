638978d3a2ca5a40a8048a857150469f2e930e31
ryougyoumei <ryougyoumei@gmail.com>
2023-02-16 08:44:56 +0900

    Homework04

---
+++ src/jp/RyouGyoumei/helloworld/src/Homework03.java
@@ -0,0 +1,15 @@
+public class Homework03 {
+    public static void main(String[] args) {
+        String book1 = "bookname01";
+        String book2 = "bookname02";
+        System.out.println(book1 + book2);
+
+        char c1 = 'M';
+        char c2 = 'W';
+        System.out.println(c1 + c2);
+
+        double price1 = 123.56;
+        double price2 = 100.11;
+        System.out.println(price1 + price2);
+    }
+}
---
+++ src/jp/RyouGyoumei/helloworld/src/Homework04.java
@@ -0,0 +1,12 @@
+public class Homework04 {
+    public static void main(String[] args) {
+        String name = "jack";
+        int age = 20;
+        double score = 80.9;
+        char gender = 'M';
+        String hobby = "play game";
+
+        System.out.println("name\tage\tscore\tgender\thobby\n" + 
+                            name + "\t" + age + "\t" + score + "\t"+ gender + "\t" + hobby);
+    }
+}

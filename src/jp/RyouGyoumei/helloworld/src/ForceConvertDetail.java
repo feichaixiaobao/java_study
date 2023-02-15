6b03cbe2853cf609dcd36a7d29a50f170e01fe9d
ryougyoumei <ryougyoumei@gmail.com>
2023-02-16 07:56:14 +0900

    ForceConvertDetail

---
+++ src/jp/RyouGyoumei/helloworld/src/ForceConvertDetail.java
@@ -0,0 +1,11 @@
+public class ForceConvertDetail {
+    public static void main(String[] args) {
+        int x = (int)(10*3.5+6*1.5);
+        System.out.println(x);
+
+        char c1 = 100;
+        int m = 100;
+        char c3 = (char)m;
+        System.out.println(c3);
+    }
+}

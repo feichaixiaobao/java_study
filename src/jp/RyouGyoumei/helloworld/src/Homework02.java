3332b4e3a60649a551bd0d4290bffd3bb0f77bf8
ryougyoumei <ryougyoumei@gmail.com>
2023-02-16 08:30:32 +0900

    Homework02

---
+++ src/jp/RyouGyoumei/helloworld/src/Homework02.java
@@ -0,0 +1,19 @@
+public class Homework02 {
+    public static void main(String[] args) {
+        char c1 = '\n'; 
+        char c2 = '\t';  
+        char c3 = '\r';  
+        char c4 = '\\';  
+        char c5 = '1';  
+        char c6 = '2';  
+        char c7 = '3';  
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+        System.out.println(c7);
+    }
+}

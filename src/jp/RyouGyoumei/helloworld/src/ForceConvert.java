86bf83bc2077077072028c147de2bb417e46609a
ryougyoumei <ryougyoumei@gmail.com>
2023-02-16 07:46:32 +0900

    ForceConvert

---
+++ src/jp/RyouGyoumei/helloworld/src/ForceConvert.java
@@ -0,0 +1,12 @@
+public class ForceConvert {
+    public static void main(String[] args) {
+
+        int n1 = (int)1.9;
+        System.out.println(i);
+
+        int j = 100;
+        byte b1 = (byte)j;
+        System.out.println(b1);
+
+        }
+}

74953ec7fc63429c0a9b3fb97a898f5952c9489e
ryougyoumei <ryougyoumei@gmail.com>
2023-02-16 08:26:56 +0900

    Homework01

---
+++ src/jp/RyouGyoumei/helloworld/src/StringToBasicDetail.java
@@ -0,0 +1,7 @@
+public class StringToBasicDetail {
+    public static void main(String[] args) {
+        String str = "hello";
+        int n1 = Integer.parseInt(str);
+        System.out.println(n1);
+    }
+}

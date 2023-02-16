0a3d66be7b8e312a89d11497c15561b508c47bfe
ryougyoumei <ryougyoumei@gmail.com>
2023-02-17 08:56:06 +0900

    AthimeticOperator

---
+++ src/jp/RyouGyoumei/Chapter 02/AthimeticOperator.java
@@ -0,0 +1,7 @@
+public class AthimeticOperator {
+    public static void main(String[] args) {
+        System.out.println(10/4);
+        double d = 10 / 4;
+        System.out.println(d);
+    }
+}

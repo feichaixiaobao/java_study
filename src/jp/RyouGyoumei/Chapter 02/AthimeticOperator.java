4b40e1aa75334d99ce27e6b23918dc59157b31fb
ryougyoumei <ryougyoumei@gmail.com>
2023-02-17 09:07:51 +0900

    update

--- src/jp/RyouGyoumei/Chapter 02/AthimeticOperator.java
+++ src/jp/RyouGyoumei/Chapter 02/AthimeticOperator.java
@@ -4,5 +4,11 @@ public class AthimeticOperator {
         double d = 10 / 4;
         System.out.println(d);
         System.out.println(10.0 / 4);
+
+        System.out.println(10 % 3);
+        System.out.println(-10 % 3);
+        System.out.println(10 % -3);
+        System.out.println(-10 % -3);
+        // 在%的本质 看一个公式 a % b = a - a / b * b
     }
 }

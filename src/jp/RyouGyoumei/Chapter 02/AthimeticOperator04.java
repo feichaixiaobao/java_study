410645733ad9517f11d7fba21e2815e2bd53d70c
ryougyoumei <ryougyoumei@gmail.com>
2023-02-18 10:40:50 +0900

    practice

---
+++ src/jp/RyouGyoumei/Chapter 02/AthimeticOperator04.java
@@ -0,0 +1,12 @@
+public class AthimeticOperator04 {
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+        int i = i1++;
+        System.out.println("i=" + i);
+        System.out.println("i2=" + i2);
+        i = --i2;
+        System.out.println("i="+i);
+        System.out.println("i2="+i2);
+    }
+}

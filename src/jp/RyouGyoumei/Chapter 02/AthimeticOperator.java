f220847cd8a11c2adea3819218638c6a0067ddd6
ryougyoumei <ryougyoumei@gmail.com>
2023-02-18 10:25:07 +0900

    next

--- src/jp/RyouGyoumei/Chapter 02/AthimeticOperator.java
+++ src/jp/RyouGyoumei/Chapter 02/AthimeticOperator.java
@@ -10,5 +10,19 @@ public class AthimeticOperator {
         System.out.println(10 % -3);
         System.out.println(-10 % -3);
         // 在%的本质 看一个公式 a % b = a - a / b * b
+
+        int i = 10;
+        i++;//自增 等价于 i = i + 1； =>  i = 11
+        ++i;//自增 等价于 i = i + 1； => i =12
+        /*
+         * 作为表达式使用
+         * 前++ ：++i先自增后赋值
+         * 后++ ：i++先赋值后自增
+         */
+        int j = 8;
+        int k = j++;
+
+        System.out.println("k=" + k + "j+" + j);
+        System.out.println("i=" + i);
     }
 }

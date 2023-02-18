29f59ad98f1848a03793c8af290259011ebc3e3b
ryougyoumei <ryougyoumei@gmail.com>
2023-02-18 14:59:31 +0900

    RelationalOperator01

---
+++ src/jp/RyouGyoumei/Chapter 02/RelationalOperator01.java
@@ -0,0 +1,13 @@
+public class RelationalOperator01 {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 8;
+        System.out.println(a >b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        boolean flag = a > b;
+        System.out.println("flag=" + flag );
+    }
+}

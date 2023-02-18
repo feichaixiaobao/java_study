2b286304d3910119a6b51a8b1d9bd89aaa9a5494
ryougyoumei <ryougyoumei@gmail.com>
2023-02-18 15:02:43 +0900

    detail

--- src/jp/RyouGyoumei/Chapter 02/RelationalOperator01.java
+++ src/jp/RyouGyoumei/Chapter 02/RelationalOperator01.java
@@ -9,5 +9,15 @@ public class RelationalOperator01 {
         System.out.println(a != b);
         boolean flag = a > b;
         System.out.println("flag=" + flag );
+
+        /*
+         * 关系运算符
+         * 
+         * //细节说明
+         * 
+         * 1）关系运算符的结果都是boolean型，也就是要么是true要么是false
+         * 2）关系运算符组成的表达式，我们称为关系表达式。a > b
+         * 3）比较运算符“==”不能误写成”=“
+         */
     }
 }

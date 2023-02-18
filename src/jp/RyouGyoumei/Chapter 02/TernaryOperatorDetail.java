5325fd05f3ca1224aa15a24c4797f89b76b1340e
ryougyoumei <ryougyoumei@gmail.com>
2023-02-18 16:55:59 +0900

    practice

---
+++ src/jp/RyouGyoumei/Chapter 02/TernaryOperatorDetail.java
@@ -0,0 +1,13 @@
+public class TernaryOperatorDetail {
+    public static void main(String[] args) {
+        // int a = 3;
+        // int b = 8;
+        // int c = a > b ? a : b;
+        // System.out.println("c=" + c);
+
+        int a = 3;
+        int b = 8;
+        int c = a > b ? (int)1.1 : (int)3.4;
+        System.out.println("c=" + c);
+    }
+}

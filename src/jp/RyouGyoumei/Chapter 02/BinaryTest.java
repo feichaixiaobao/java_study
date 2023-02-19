f3035f9f99d6968f55b4441b868bd90556ed793f
ryougyoumei <ryougyoumei@gmail.com>
2023-02-19 13:46:57 +0900

    BinaryTest

---
+++ src/jp/RyouGyoumei/Chapter 02/BinaryTest.java
@@ -0,0 +1,18 @@
+public class BinaryTest {
+    public static void main(String[] args) {
+        //n1 二进制
+        int n1 = 0b1010;
+        //n2 十进制
+        int n2 = 1010;
+        //n3 八进制
+        int n3 = 01010;
+        //n4 十六进制
+        int n4 = 0x10101;
+
+        System.out.println("n1=" + n1);
+        System.out.println("n2=" + n2);
+        System.out.println("n3=" + n3);
+        System.out.println("n4=" + n4);
+
+    }
+}

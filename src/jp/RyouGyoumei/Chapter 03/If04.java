28243d29f601788b858a597f617dec4ad1167608
ryougyoumei <ryougyoumei@gmail.com>
2023-02-20 09:24:30 +0900

    if else practice

---
+++ src/jp/RyouGyoumei/Chapter 03/If04.java
@@ -0,0 +1,12 @@
+public class If04 {
+    public static void main(String[] args) {
+        double a = 10.0;
+        double b = 20.0;
+
+        if (a > 10.0 && b <20.0) {
+            System.out.println("Y");
+        } else {
+            System.out.println("N");
+        }
+    }
+}

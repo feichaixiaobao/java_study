86c41bdf669ee53c5d68f5f60f7768063b825727
ryougyoumei <ryougyoumei@gmail.com>
2023-02-22 16:56:15 +0900

    next

--- src/jp/RyouGyoumei/Chapter 03/NestedIf02.java
+++ src/jp/RyouGyoumei/Chapter 03/NestedIf02.java
@@ -1,27 +1,37 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class NestedIf02 {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("请输入月份：");
-        int mounth = myScanner.nextInt();
-        System.out.println("请输入年龄：");
-        int age = myScanner.nextInt();
+        Scanner myScanner = null;
+        try {
+            myScanner = new Scanner(System.in);
+            System.out.println("请输入月份：");
+            int mounth = myScanner.nextInt();
+            System.out.println("请输入年龄：");
+            int age = myScanner.nextInt();
 
-        if (3 < mounth && mounth< 11) {
-            if ( 0 < age && age < 18) {
-                System.out.println("票价：半价");
-            } else if ( 17 < age && age < 61){
-                System.out.println("票价：60");
+            if (3 < mounth && mounth < 11) {
+                if (0 < age && age < 18) {
+                    System.out.println("票价：半价");
+                } else if (17 < age && age < 61) {
+                    System.out.println("票价：60");
+                } else if (60 < age) {
+                    System.out.println("票价：1/3");
+                }
+            } else {
+                if (17 < age && age < 61) {
+                    System.out.println("票价：40");
+                } else {
+                    System.out.println("票价：20");
+                }
             }
-            else if ( 60 < age ){
-                System.out.println("票价：1/3");
-            }
-        } else {
-            if ( 17 < age && age < 61){
-                System.out.println("票价：40");
-            }
-            else {
-                System.out.println("票价：20");
+        } catch (InputMismatchException e) {
+            System.out.println("输入格式错误！");
+            e.printStackTrace();
+        } finally {
+            if (myScanner != null) {
+                myScanner.close();
             }
         }
     }

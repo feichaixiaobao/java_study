d83b027b0f46675491a3a50a7fd7952bb044dde3
ryougyoumei <ryougyoumei@gmail.com>
2023-02-22 16:55:55 +0900

    swithchdetail

--- src/jp/RyouGyoumei/Chapter 03/Swithch01.java
+++ src/jp/RyouGyoumei/Chapter 03/Swithch01.java
@@ -1,35 +1,42 @@
 import java.util.Scanner;
-import java.util.myScanner;
 public class Swithch01 {
     public static void main(String[] args) {
         Scanner myScanner= new Scanner(System.in);
-        System.out.println("请输入一个字符（a-g");
-        char c1 = myScanner.next.charAt9();
+        System.out.println("请输入一个字符（a-g）");
+        String input = myScanner.next();
+        if (input.length() != 1 || input.charAt(0) < 'a' || input.charAt(0) > 'g') {
+            System.out.println("非法输入");
+            return;
+        }
+        char c1 = input.charAt(0);
         
         switch(c1){
             case 'a' :
-            System.out.println("Monday");
-            break;
+                System.out.println("Monday");
+                break;
             case 'b' :
-            System.out.println("Tuesday");
-            break;
+                System.out.println("Tuesday");
+                break;
             case 'c' :
-            System.out.println("Wendnesday");
-            break;
+                System.out.println("Wednesday");
+                break;
             case 'd' :
-            System.out.println("Thursday");
-            break;
+                System.out.println("Thursday");
+                break;
             case 'e' :
-            System.out.println("Friday");
-            break;
+                System.out.println("Friday");
+                break;
             case 'f' :
-            System.out.println("Saturday");
-            break;
+                System.out.println("Saturday");
+                break;
             case 'g' :
-            System.out.println("Sunday");
-            break;
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Erro!type again!");
+                break;
         }
         System.out.println("game over");
-
     }
 }

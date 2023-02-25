09254ceb80b81b755045fcd5575d7ba81483d02d
ryougyoumei <ryougyoumei@gmail.com>
2023-02-26 01:11:16 +0900

    next

--- src/jp/RyouGyoumei/Chapter 03/ForDetail.java
+++ src/jp/RyouGyoumei/Chapter 03/ForDetail.java
@@ -4,24 +4,24 @@ public class ForDetail {
         //中的初始化和变量迭代可以写到其他地方，但是两边的分号不能省略
 
         //使用for循环控制
-        int i = 1;//循环变量初始化
-        for (; i <= 10;) {
-            System.out.println("A" + i);
-            i++;
+        // int i = 1;//循环变量初始化
+        // for (; i <= 10;) {
+        //     System.out.println("A" + i);
+        //     i++;
 
-            //补充
-            for (;;) {  //表示一个无限死循环
+        //     //补充
+        //     for (;;) {  //表示一个无限死循环
     
-                System.out.println("Yes");
-            }
+        //         System.out.println("Yes");
+        //     }
 
             //循环初始值可以有多条初始化语句，但要求类似一样，
             //并且中间用逗号离开，循环变量迭代也可以有多条变量
 
             int count = 3;
             for (int a = 0, j = 0; a < count; a++, j += 2 ) {
-                System.out.println("i=" + i + "j=" + j);
+                System.out.println("i=" + a + "j=" + j);
             }
         }
     }
-}
+

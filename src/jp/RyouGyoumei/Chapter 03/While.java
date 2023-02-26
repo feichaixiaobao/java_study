73f8e8c74b45fb42dc515d38fa646f74a910d60e
ryougyoumei <ryougyoumei@gmail.com>
2023-02-26 10:55:26 +0900

    While

--- src/jp/RyouGyoumei/Chapter 03/While.java
+++ src/jp/RyouGyoumei/Chapter 03/While.java
@@ -1,5 +1,10 @@
 public class While {
     public static void main(String[] args) {
-        
+        int i = 1;//循环变量初始化
+        while ( i <= 10 ) {//循环条件
+            System.out.println("hello" + i);//执行
+            i++;//循环变量迭代
+        }
+        System.out.println("out");
     }
 }

b9705fd4467fe9c4458f7c4df16e1fade220b833
ryougyoumei <ryougyoumei@gmail.com>
2023-02-28 09:40:04 +0900

    DoWhile04

--- src/jp/RyouGyoumei/Chapter 03/DoWhile04.java
+++ src/jp/RyouGyoumei/Chapter 03/DoWhile04.java
@@ -1,14 +1,18 @@
+import javax.xml.stream.events.StartDocument;
+
 public class DoWhile04 {
     public static void main(String[] args) {
-        int i = 1;
         int count = 0;
+        int end = 200;
+        int start = 1;
+        int i = start;
         do {
             if (i % 5 ==0 && i % 3 != 0) {
                 System.out.println(i);
                 count ++;
             }
             i++;
-        } while (i <= 200);
+        } while (i <= end);
         System.out.println("conunt=" + count);
     }
 }

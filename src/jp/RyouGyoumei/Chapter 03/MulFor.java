a62069bde96c1cc212ad1db574d19f60c9013407
ryougyoumei <ryougyoumei@gmail.com>
2023-03-02 00:23:17 +0900

    MulFor

---
+++ src/jp/RyouGyoumei/Chapter 03/MulFor.java
@@ -0,0 +1,9 @@
+public class MulFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("i=" +  i + "j=" + j);
+            }
+        }
+    }
+}

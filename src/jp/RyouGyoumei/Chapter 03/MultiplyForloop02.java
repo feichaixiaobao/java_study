56b2acf5ef1bdbdfbc01e1bbcedab22c687de6a8
ryougyoumei <ryougyoumei@gmail.com>
2023-03-03 00:49:20 +0900

    test

--- src/jp/RyouGyoumei/Chapter 03/MultiplyForloop02.java
+++ src/jp/RyouGyoumei/Chapter 03/MultiplyForloop02.java
@@ -2,7 +2,7 @@ public class MultiplyForloop02 {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.println(i + "x" + j + "=" + i*j + "\t");
+                System.out.print(i + "x" + j + "=" + i*j + "\t");
             }
             System.out.println();
         }
--- src/jp/RyouGyoumei/Chapter 03/Star.java
+++ src/jp/RyouGyoumei/Chapter 03/Star.java
@@ -1,5 +1,10 @@
 public class Star {
     public static void main(String[] args) {
-        
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\t");
+        }
     }
 }

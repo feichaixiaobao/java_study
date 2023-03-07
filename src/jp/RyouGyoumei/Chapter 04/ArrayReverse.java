48351dc87e2602022f7dfc6e35f8225953eb58b7
ryougyoumei <ryougyoumei@gmail.com>
2023-03-07 09:13:32 +0900

    アレイの反転

---
+++ src/jp/RyouGyoumei/Chapter 04/ArrayReverse.java
@@ -0,0 +1,18 @@
+public class ArrayReverse {
+    public static void main(String[] args) {
+        int[] arr = {11,22,33,44,55,66};
+        //思路
+        //1. 把arr[0]和 arr[5]进行交换 {66,22,33,44,55,11}
+        //2. 把arr[1]和 arr[4]进行交换 {66,55,33,44,22,11}
+        //3. 把arr[2]和 arr[3]进行交换 {66,55,44,33,22,11}
+        //4. 一共要交换 3 次 = arr.lenth / 2
+        //5. 每次交换时，对应的下标是 arr[i] 和 arr[arr.length - 1 - i]
+        int temp = 0;
+        int len = arr.length;//计算数组的长度
+        for (int i = 0; i < arr.length / 2; i++) {
+            temp = arr[len - 1 - i];//保存
+            arr[len - 1 -i] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
